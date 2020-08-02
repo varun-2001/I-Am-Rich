d9fd33b985ebc24e8021c6c3a77dd56b96f3b114
varun-2001 <57247403+varun-2001@users.noreply.github.com>
2020-08-03 03:52:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+ import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title:Center(
+            child:Text('I Am Rich')
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    ),
+  )
+  );
+}
+
